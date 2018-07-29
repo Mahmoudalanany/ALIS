9dbb8ccb31a534fd0a3b979289ed50f2138f8b6d
Fady Nasser <fady_nasser9@hotmail.com>
2018-07-29 16:04:25 +0200

    Add files via upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component , NgZone } from '@angular/core';
+import { NavController , Platform } from 'ionic-angular';
+
+declare var ApiAIPromises: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  answers = [];
+  CurrentTime;
 
+  constructor(public navCtrl: NavController, public platform: Platform, public ngZone: NgZone) {
+    var hours = new Date().getHours()
+    var minutes = new Date().getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    var minutesupdated = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutesupdated + ' ' + ampm;
+    this.CurrentTime = strTime;
+
+    platform.ready().then(() => {
+      ApiAIPromises.init({
+        clientAccessToken: "294dbb12e7f242119a3501a34da745ef"
+      }).then(result => console.log(result));
+    }).catch(e => {console.log(e);});
+  }
+
+  ask(question) {
+    ApiAIPromises.requestText({
+      query: question
+    })
+      .then(({ result: { fulfillment: { speech } } }) => {
+        this.ngZone.run(() => {
+          this.answers.push(speech);
+        });
+      }).catch(e => {
+        console.log(e);
+      })  
   }
 
 }
