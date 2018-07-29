8eb8a487c6e65421e2a154363ef72b143ac36ec0
Fady Nasser <fady_nasser9@hotmail.com>
2018-07-29 16:18:11 +0200

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,35 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
+import { FCM } from '@ionic-native/fcm';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private fcm: FCM) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
+      //Notifications
+      fcm.subscribeToTopic('all');
+      fcm.getToken().then(token=>{
+          console.log(token);
+      })
+      fcm.onNotification().subscribe(data=>{
+        if(data.wasTapped){
+          console.log("Received in background");
+        } else {
+          console.log("Received in foreground");
+        };
+      })
+      fcm.onTokenRefresh().subscribe(token=>{
+        console.log(token);
+      });
+      //end notifications.
+       statusBar.styleDefault();
+       splashScreen.hide();
+     });
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { FCM } from '@ionic-native/fcm';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +26,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    FCM,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
