740cdbed0db46bef6eddf9dba0754d153b1249c9
Ahmed Abd El-Hamid <ahmed_a.mahmoud@hotmail.com>
2018-08-08 19:56:05 +0200

    SignUp

--- src/pages/app/firebase.credentials.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyB2okykOVc15jxtjAL_jArbP9JwE963ejU",
-    authDomain: "alis-ac07d.firebaseapp.com",
-    databaseURL: "https://alis-ac07d.firebaseio.com",
-    projectId: "alis-ac07d",
-    storageBucket: "alis-ac07d.appspot.com",
-    messagingSenderId: "172006516486"
-}
